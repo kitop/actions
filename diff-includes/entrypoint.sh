befe49aa73115254b6a5ddc1c52919770b580870
Esteban Pastorino <ejpastorino@gmail.com>
2018-10-05 16:47:26 +0200

    update ignore file

--- diff-includes/entrypoint.sh
+++ diff-includes/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "if ! git diff-index --quiet HEAD~$(jq '.commits | length' "${GITHUB_EVENT_PATH}") $*; then echo 'changes in $*, proceeding'; else echo 'no changes in $*, stopping' && touch $HOME/noop; fi"
+sh -c "if ! git diff-index --quiet HEAD~$(jq '.commits | length' "${GITHUB_EVENT_PATH}") $*; then echo 'changes in $*, proceeding'; else echo 'no changes in $*, stopping' && echo 'ignore:$*' >> $HOME/ignore; fi"

c3fd06bdf0d37dcd4d6e0b01c84a4c3bec9766f9
Esteban Pastorino <ejpastorino@gmail.com>
2018-10-05 19:28:00 +0200

    add params

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "if grep \"^ignore:$BUILD_DIR\" $HOME/ignore; then echo \"$BUILD_DIR didn't change\"; else curl -H \"Authorization: $GITHUB_TOKEN\" -H \"X-GitHub-Event: $GITHUB_EVENT\" -H 'Content-Type: application/json' --data-binary \"@$GITHUB_EVENT_PATH\" https://api.netlify.com/api/v1/github/$GITHUB_REPOSITORY/build ; fi"
+sh -c "if grep \"^ignore:$BUILD_DIR\" $HOME/ignore; then echo \"$BUILD_DIR didn't change\"; else curl -H \"Authorization: $GITHUB_TOKEN\" -H \"X-GitHub-Event: $GITHUB_EVENT\" -H 'Content-Type: application/json' --data-binary \"@$GITHUB_EVENT_PATH\" https://api.netlify.com/api/v1/github/$GITHUB_REPOSITORY/build?cmd=$NETLIFY_CMD&base=$NETLIFY_BASE&dir=$NETLIFY_DIR ; fi"

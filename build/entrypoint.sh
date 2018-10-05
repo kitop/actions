4a9bac16e4916545640713ab34587dc731826b43
Esteban Pastorino <ejpastorino@gmail.com>
2018-10-05 20:38:34 +0200

    more complex build

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,3 +1,19 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-sh -c "if [[ -f $HOME/ignore ]] && grep \"^ignore:$BUILD_DIR\" $HOME/ignore; then echo \"$BUILD_DIR didn't change\"; else echo 'Deploying' && curl -H \"Authorization: $GITHUB_TOKEN\" -H \"X-GitHub-Event: $GITHUB_EVENT\" -H 'Content-Type: application/json' --data-binary \"@$GITHUB_EVENT_PATH\" https://api.netlify.com/api/v1/github/$GITHUB_REPOSITORY/build?cmd=$NETLIFY_CMD&base=$NETLIFY_BASE&dir=$NETLIFY_DIR ; fi"
+if [[ -f $HOME/ignore ]] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
+  echo "$BUILD_DIR didn't change"
+else
+  echo 'Deploying'
+  jq \
+    --arg cmd "$NETLIFY_CMD" \
+    --arg base "$NETLIFY_BASE" \
+    --arg dir "$NETLIFY_DIR" \
+    '. + {cmd: $cmd, base: $base, dir: $dir}' \
+     "$GITHUB_EVENT_PATH" > args.json
+  curl \
+    -H "Authorization: $GITHUB_TOKEN" \
+    -H "X-GitHub-Event: $GITHUB_EVENT" \
+    -H 'Content-Type: application/json' \
+    --data-binary @args.json \
+    "https://api.netlify.com/api/v1/github/$GITHUB_REPOSITORY/build"
+fi
